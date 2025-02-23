615ade1716cbddd5947f1a36d0b81a2daa6d197f
deepak <deepaktirumalsetty25@gmail.com>
2025-02-23 14:07:19 +0530

    fixed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const connectToDb = async () => {
 connectToDb();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
